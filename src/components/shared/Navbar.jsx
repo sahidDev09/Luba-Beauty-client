a4422807215b2942748a2929e2b3c075279a7e43
sahidDev09 <iamsahidofficial99@gmail.com>
2024-08-15 12:06:46 +0600

    navbar responsive

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,76 +2,140 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="bg-pink-50">
-      <div className="container mx-auto flex justify-between items-center py-2">
-        <div className="">
-          <img
-            className="w-28"
-            src="https://i.ibb.co/58dnSPv/COSMETICS-1.png"
-            alt="Logo"
-          />
-        </div>
-        <div className="">
-          <ul className="flex items-center gap-10">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
-              }>
-              Home
-            </NavLink>
-            <NavLink
-              to="/products"
-              className={({ isActive }) =>
-                isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
-              }>
-              Products
-            </NavLink>
-            <NavLink
-              to="/outlets"
-              className={({ isActive }) =>
-                isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
-              }>
-              Outlets
-            </NavLink>
-          </ul>
-        </div>
-        <div className="flex gap-2">
-          <div className="form-control">
-            <input
-              type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
-            />
-          </div>
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+    <div className=" bg-pink-50">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
                 />
-              </div>
+              </svg>
             </div>
             <ul
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? " bg-pink-200 px-4 py-2 rounded-md"
+                      : "text-black"
+                  }>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <a>Settings</a>
+                <NavLink
+                  to="/products"
+                  className={({ isActive }) =>
+                    isActive
+                      ? " bg-pink-200 px-4 py-2 rounded-md"
+                      : "text-black"
+                  }>
+                  Products
+                </NavLink>
               </li>
               <li>
-                <a>Logout</a>
+                <NavLink
+                  to="/outlets"
+                  className={({ isActive }) =>
+                    isActive
+                      ? " bg-pink-200 px-4 py-2 rounded-md"
+                      : "text-black"
+                  }>
+                  Outlets
+                </NavLink>
               </li>
             </ul>
+          </div>
+          <NavLink>
+            <img
+              className=" w-16 md:w-24"
+              src="https://i.ibb.co/58dnSPv/COSMETICS-1.png"
+              alt="logo"
+            />
+          </NavLink>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
+                }>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/products"
+                className={({ isActive }) =>
+                  isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
+                }>
+                Products
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/outlets"
+                className={({ isActive }) =>
+                  isActive ? " bg-pink-200 px-4 py-2 rounded-md" : "text-black"
+                }>
+                Outlets
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <div className="flex gap-2">
+            <div className="form-control hidden md:inline">
+              <input
+                type="text"
+                placeholder="Search"
+                className="input input-bordered w-24 md:w-auto"
+              />
+            </div>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar">
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                <li>
+                  <a className="justify-between">
+                    Profile
+                    <span className="badge">New</span>
+                  </a>
+                </li>
+                <li>
+                  <a>Settings</a>
+                </li>
+                <li>
+                  <a>Logout</a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>

8e109502f80d2c5516bcfc8dc7106568986836b6
sahidDev09 <iamsahidofficial99@gmail.com>
2024-08-17 13:07:42 +0600

    fix mobile responsive

--- src/pages/Produtcs.jsx
+++ src/pages/Produtcs.jsx
@@ -12,11 +12,12 @@ const Products = () => {
   const [minPrice, setMinPrice] = useState(0);
   const [maxPrice, setMaxPrice] = useState(500);
   const [totalPages, setTotalPages] = useState(1);
+  const [sortBy, setSortBy] = useState("priceAsc");
   const productsPerPage = 6;
 
   const fetchProducts = (page = 1) => {
     fetch(
-      `http://localhost:8000/products?page=${page}&limit=${productsPerPage}&search=${searchQuery}&category=${selectedCategory}&brand=${selectedBrand}&minPrice=${minPrice}&maxPrice=${maxPrice}`
+      `http://localhost:8000/products?page=${page}&limit=${productsPerPage}&search=${searchQuery}&category=${selectedCategory}&brand=${selectedBrand}&minPrice=${minPrice}&maxPrice=${maxPrice}&sortBy=${sortBy}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -39,30 +40,23 @@ const Products = () => {
     selectedBrand,
     minPrice,
     maxPrice,
+    sortBy,
   ]);
-
-  // category func
 
   const handleCategoryChange = (e) => {
     setSelectedCategory(e.target.value);
     setCurrentPage(1);
   };
 
-  //brand  fucn
-
   const handleBrandChange = (e) => {
     setSelectedBrand(e.target.value);
     setCurrentPage(1);
   };
 
-  //search func
-
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
     setCurrentPage(1);
   };
-
-  //price range code
 
   const handleMinPriceChange = (e) => {
     setMinPrice(e.target.value);
@@ -74,7 +68,10 @@ const Products = () => {
     setCurrentPage(1);
   };
 
-  //pagination func
+  const handleSortChange = (e) => {
+    setSortBy(e.target.value);
+    setCurrentPage(1);
+  };
 
   const handlePageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
@@ -84,48 +81,46 @@ const Products = () => {
 
   return (
     <div className="bg-pink-50 min-h-screen">
-      {/* Filtering UI */}
       <div className="container mx-auto py-4">
-        <div className="flex justify-between items-center">
-          {/* Category Filter */}
-          <div>
-            <select
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-              className="select select-bordered">
-              <option value="all">All Categories</option>
-              <option value="Skincare">Skincare</option>
-              <option value="Boys Skincare">Boys Skincare</option>
-              <option value="Hair Treatment">Hair Treatment</option>
-              <option value="Makeup">Makeup</option>
-              <option value="Body Wash">Body Wash</option>
-              <option value="Earrings">Earrings</option>
-              <option value="Necklace">Necklace</option>
-            </select>
+        <div className="md:flex items-center justify-between ">
+          {/* Category Filter && brand */}
+          <div className=" flex items-center gap-4  w-fit mx-auto">
+            <div>
+              <select
+                value={selectedCategory}
+                onChange={handleCategoryChange}
+                className="select select-bordered">
+                <option value="all">All Categories</option>
+                <option value="Skincare">Skincare</option>
+                <option value="Boys Skincare">Boys Skincare</option>
+                <option value="Hair Treatment">Hair Treatment</option>
+                <option value="Makeup">Makeup</option>
+                <option value="Body Wash">Body Wash</option>
+                <option value="Earrings">Earrings</option>
+                <option value="Necklace">Necklace</option>
+              </select>
+            </div>
+
+            <div>
+              <select
+                value={selectedBrand}
+                onChange={handleBrandChange}
+                className="select select-bordered">
+                <option value="all">All Brands</option>
+                <option value="GlowSkin">GlowSkin</option>
+                <option value="LipCharm">LipCharm</option>
+                <option value="ManCare">ManCare</option>
+                <option value="AquaClean">AquaClean</option>
+                <option value="HairGlow">HairGlow</option>
+                <option value="EleganceJewels">EleganceJewels</option>
+                <option value="LuxeGems">LuxeGems</option>
+                <option value="ScrubFresh">ScrubFresh</option>
+              </select>
+            </div>
           </div>
 
-          {/* brand code */}
-
-          <div>
-            <select
-              value={selectedBrand}
-              onChange={handleBrandChange}
-              className="select select-bordered">
-              <option value="all">All Brands</option>
-              <option value="GlowSkin">GlowSkin</option>
-              <option value="LipCharm">LipCharm</option>
-              <option value="ManCare">ManCare</option>
-              <option value="AquaClean">AquaClean</option>
-              <option value="HairGlow">HairGlow</option>
-              <option value="EleganceJewels">EleganceJewels</option>
-              <option value="LuxeGems">LuxeGems</option>
-              <option value="ScrubFresh">ScrubFresh</option>
-            </select>
-          </div>
-
-          {/* price range min to max slider */}
-
-          <div className=" flex gap-3 items-center">
+          {/* Price Range */}
+          <div className="flex gap-3 items-center w-fit mx-auto my-2">
             <div className="flex items-center gap-1">
               <label htmlFor="">Min</label>
               <input
@@ -148,26 +143,39 @@ const Products = () => {
             </div>
           </div>
 
-          {/* Search bar */}
-          <div className="flex items-center gap-2">
-            <input
-              type="text"
-              className="input input-bordered focus:outline-none"
-              placeholder="Search"
-              value={searchQuery}
-              onChange={handleSearchChange}
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              className="h-4 w-4 opacity-70">
-              <path
-                fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd"
+          {/* Sorting && serach */}
+          <div className="md:flex gap-4 items-center w-fit mx-auto">
+            <div className="flex items-center gap-2 my-2">
+              <select
+                value={sortBy}
+                onChange={handleSortChange}
+                className="select select-bordered bg-green-50 focus:outline-none">
+                <option value="priceAsc">Price: Low to High</option>
+                <option value="priceDesc">Price: High to Low</option>
+                <option value="dateAdded">Date Added: Newest First</option>
+              </select>
+            </div>
+
+            <div className="flex items-center gap-2">
+              <input
+                type="text"
+                className="input input-bordered focus:outline-none"
+                placeholder="Search"
+                value={searchQuery}
+                onChange={handleSearchChange}
               />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="h-4 w-4 opacity-70">
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
@@ -190,9 +198,9 @@ const Products = () => {
             ))}
           </div>
           {/* Pagination Controls */}
-          <div className="flex justify-center py-4 ">
+          <div className="flex justify-center py-4">
             <button
-              className="btn bg-[#e58c8a] text-white "
+              className="btn bg-[#e58c8a] text-white"
               onClick={() => handlePageChange(currentPage - 1)}
               disabled={currentPage === 1}>
               Previous
